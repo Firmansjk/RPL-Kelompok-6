f8195f599424e8c2e27b5e3cc68efd87b677bd30
unknown <hardiantots>
2023-04-16 21:46:25 +0800

    admin page untuk ditambahkan ke laravel

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import LoginPage from './pages/loginpage';
+import RegisterPage from './pages/registerpage';
+import ProfilePage from './pages/profile';
+import MenuPage from './pages/menupage';
+import LupaPassPage from './pages/lupaPass';
+
+
+function App() {
+  return (
+      <Router>
+        <Routes>
+            <Route exact path='/' element={<LoginPage />} />
+            <Route path='/register' element={<RegisterPage/>} />
+            <Route path='/resetpassword' element={<LupaPassPage/>}/>
+
+            <Route path='/profile' element={<ProfilePage/>}/>
+            <Route path='/menu' element={<MenuPage/>}/>
+        </Routes>
+      </Router>
+  );
+}
+  
+export default App;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,20 +1,14 @@
-const defaultTheme = require('tailwindcss/defaultTheme');
-
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: [
-        './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
-        './storage/framework/views/*.php',
-        './resources/views/**/*.blade.php',
-    ],
-
-    theme: {
-        extend: {
-            fontFamily: {
-                sans: ['Figtree', ...defaultTheme.fontFamily.sans],
-            },
-        },
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        Montserrat : ['Montserrat', 'sans-serif'],
+      }
     },
-
-    plugins: [require('@tailwindcss/forms')],
-};
+  },
+  plugins: [],
+}
