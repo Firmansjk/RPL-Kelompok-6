61d47e78dfcf0393aec9819317c0feb4b4fda886
Muhammad Fadhil Akbar Amrin <90586274+FadhilAk1@users.noreply.github.com>
2023-06-13 00:16:40 +0800

    fix problems 3

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -9,6 +9,7 @@ window._ = _;
 
 import axios from 'axios';
 window.axios = axios;
+window.axios.defaults.headers.common['X-CSRF-TOKEN'] = token;
 
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 
